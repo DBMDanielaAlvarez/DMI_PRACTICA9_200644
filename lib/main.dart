8f00165eea7bbe5732504194f05e99b8487feaec
Daniela06112002 <84827002+Daniela06112002@users.noreply.github.com>
2023-10-06 03:02:50 -0600

    ADD PRACTICA9

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:movieapp_200644/home.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Home(),
+  ));
+}
